3fbb9842aad6130e3b48c432305d8a4cf97bc356
Alex Manukyan <alex@gors-macbook-pro-2.home>
2019-12-10 11:30:24 +0100

    Comment in Grace.c

--- C/Grace/Grace.c
+++ C/Grace/Grace.c
@@ -1,7 +1,10 @@
+/*
+** This program writes a copy of its own source code in a file
+*/
 #include <fcntl.h>
 #include <stdio.h>
 #include <unistd.h>
 #define FILE_NAME "Grace_kid.c"
-#define STR "#include <fcntl.h>%1$c#include <stdio.h>%1$c#include <unistd.h>%1$c#define FILE_NAME %2$c%4$s%2$c%1$c#define STR %2$c%3$s%2$c%1$c#define FT(file_name) int main(){int fd;if ((fd = open(file_name, O_WRONLY | O_CREAT | O_TRUNC, 0644)) > 0){dprintf(fd,STR,10,34,STR,FILE_NAME); close(fd); } return (0); }%1$cFT(FILE_NAME)%1$c"
+#define STR "/*%1$c** This program writes a copy of its own source code in a file%1$c*/%1$c#include <fcntl.h>%1$c#include <stdio.h>%1$c#include <unistd.h>%1$c#define FILE_NAME %2$c%4$s%2$c%1$c#define STR %2$c%3$s%2$c%1$c#define FT(file_name) int main(){int fd;if ((fd = open(file_name, O_WRONLY | O_CREAT | O_TRUNC, 0644)) > 0){dprintf(fd,STR,10,34,STR,FILE_NAME); close(fd); } return (0); }%1$cFT(FILE_NAME)%1$c"
 #define FT(file_name) int main(){int fd;if ((fd = open(file_name, O_WRONLY | O_CREAT | O_TRUNC, 0644)) > 0){dprintf(fd,STR,10,34,STR,FILE_NAME); close(fd); } return (0); }
 FT(FILE_NAME)
