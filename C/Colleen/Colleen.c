86f36785161e0bce9b80894729230eb9d843c531
Alex Manukyan <alex@gors-macbook-pro-2.home>
2019-12-10 11:23:12 +0100

    3rd define in Grace.c

--- C/Colleen/Colleen.c
+++ C/Colleen/Colleen.c
@@ -5,7 +5,7 @@
 
 void print_self()
 {
-    char *str = "#include <stdio.h>%1$c/*%1$c** This program prints a copy of its own source code%1$c*/%1$c%1$cvoid print_self()%1$c{%1$c    char *str = %2$c%3$s%2$c;%1$c    printf(str, 10, 34, str);%1$c}%1$c%1$cint main(void)%1$c{%1$c    /*%1$c    ** Print Self%1$c    */%1$c    colleen();%1$c    return (0);%1$c}";
+    char *str = "#include <stdio.h>%1$c/*%1$c** This program prints a copy of its own source code%1$c*/%1$c%1$cvoid print_self()%1$c{%1$c    char *str = %2$c%3$s%2$c;%1$c    printf(str, 10, 34, str);%1$c}%1$c%1$cint main(void)%1$c{%1$c    /*%1$c    ** Print Self%1$c    */%1$c    print_self();%1$c    return (0);%1$c}";
     printf(str, 10, 34, str);
 }
 
--- C/Grace/Grace.c
+++ C/Grace/Grace.c
@@ -1,6 +1,7 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <unistd.h>
-#define STR "#include <fcntl.h>%1$c#include <stdio.h>%1$c#include <unistd.h>%1$c#define STR %2$c%3$s%2$c%1$c#define FT() int main(){int fd;if ((fd = open(%2$cGrace_kid.c%2$c, O_WRONLY | O_CREAT | O_TRUNC, 0644)) > 0){dprintf(fd,STR,10,34,STR); close(fd); } return (0); }%1$cFT()"
-#define FT() int main(){int fd;if ((fd = open("Grace_kid.c", O_WRONLY | O_CREAT | O_TRUNC, 0644)) > 0){dprintf(fd,STR,10,34,STR); close(fd); } return (0); }
-FT()
+#define FILE_NAME "Grace_kid.c"
+#define STR "#include <fcntl.h>%1$c#include <stdio.h>%1$c#include <unistd.h>%1$c#define FILE_NAME %2$c%4$s%2$c%1$c#define STR %2$c%3$s%2$c%1$c#define FT(file_name) int main(){int fd;if ((fd = open(file_name, O_WRONLY | O_CREAT | O_TRUNC, 0644)) > 0){dprintf(fd,STR,10,34,STR,FILE_NAME); close(fd); } return (0); }%1$cFT(FILE_NAME)%1$c"
+#define FT(file_name) int main(){int fd;if ((fd = open(file_name, O_WRONLY | O_CREAT | O_TRUNC, 0644)) > 0){dprintf(fd,STR,10,34,STR,FILE_NAME); close(fd); } return (0); }
+FT(FILE_NAME)
